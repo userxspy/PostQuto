7fbdde161dee0f1bde14f0655ac435d74bfa61eb
userxspy <userxspy@gmail.com>
2026-06-04 10:11:56 +0530

    Add admin routes and registration function
    Define admin routes and register them with the application.

---
+++ web/admin_routes.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+from web.login_routes import login_routes
+from web.dashboard_routes import dashboard_routes
+from web.stats_routes import stats_routes
+from web.profile_routes import profile_routes
+
+# एक मास्टर राउट टेबल डिफाइन करें
+admin_routes = web.RouteTableDef()
+
+def register_admin_components(app: web.Application):
+    """सारे अलग-अलग फाइलों के रूट्स को सिंगल सर्वर इंजन में सिंक करने की मास्टर पाइपलाइन"""
+    app.add_routes(login_routes)
+    app.add_routes(dashboard_routes)
+    app.add_routes(stats_routes)
+    app.add_routes(profile_routes)
+    app.add_routes(admin_routes)
